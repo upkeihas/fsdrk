5df1e555defbcebe096222392f032a38b8c8b437
U-P Keihäs <upkeihas@gmail.com>
2017-11-14 10:11:40 +0200

    Reactified
    Woo reakt-projekti

---
+++ kopiogram/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center"> 
+	<div class="gallerycontainer">
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	  <div class="galleryelement col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="images/365x365.png"><img src="images/365x365.png"></img></a></div>
+	</div>
+</div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
