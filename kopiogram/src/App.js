8e8a24943c173b43f57c2f0606bd9025c3309b0e
Ukko-Pekka Keihäs <upkeihas@gmail.com>
2017-11-18 13:24:04 +0200

    Added login functions to main app

--- kopiogram/src/App.js
+++ kopiogram/src/App.js
@@ -5,9 +5,57 @@ import UserProfile from './components/UserProfile';
 import ImageGallery from './components/ImageGallery';
 
 class App extends Component {
+
+// LOGIN-JUTUT
+
+/*	constructor(props){
+		super(props);
+		this.state = {
+				isLogged:false,
+				token:""
+		}
+		this.onLogin = this.onLogin.bind(this);
+		this.onLogout = this.onLogout.bind(this);
+	}
+
+
+	onLogin(token){
+		this.setState({
+			isLogged:true,
+			token:token
+		});
+	}
+
+	onLogout(){
+		let logoutFetch={
+		method: "POST",
+		headers:{"Content-Type":"application/json"},
+		mode:"cors",
+		body:JSON.stringify({"token":this.state.token})
+		}
+		
+		fetch("/logout",logoutFetch).then((response)=>{
+			if(response.ok){
+				response.json().then((data)=>{
+					console.log(data);
+					this.setState({
+						isLogged:false
+					})
+				});
+			}
+		});
+	}
+
+*/	
+	
+	
   render() {
     return (
 		<div>
+		
+/*	Gör så här:
+		<NavigationBar isLogged={this.state.isLogged} onLogout={this.onLogout}/>
+*/
 		<NavigationBar></NavigationBar>
 		<UserProfile></UserProfile>
 		<ImageGallery></ImageGallery>
