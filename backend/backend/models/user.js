ed4deabebbd5c9f62615a9c1aa06551806640bee
mattuo <32922338+mattuo@users.noreply.github.com>
2017-11-22 08:21:35 +0200

    Added mongoose models

---
+++ backend/backend/models/user.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
+
+var schema = mongoose.Schema({
+	username: String, //{ type:String, index:true, required:true,	unique:true},
+	email: String, //{ type:String, index:true, required:true },
+  password: String, //{ type:String, required:true },
+	role: String, //{	type:String, required:true },
+	profileImage: String,
+  images: [String],
+	// follower and following must be move to separate table if over 10 000 users.
+  following: [String],
+  follower: [String]
+});
+
+schema.methods.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
+schema.methods.isPasswordValid = function(password) {
+	console.log("compareSync "+password+" "+this.password);
+	console.log(this);
+	return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model("User", schema);
