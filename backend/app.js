3f9053db1eaa32145a5e0825a5d23601f5162079
mattuo <32922338+mattuo@users.noreply.github.com>
2017-11-22 08:23:21 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,134 +1,159 @@
 let express = require("express");
-let app = express();
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let bluebird = require("bluebird");
-let contact = require("./backend/models/contact");
-
-bluebird.promisifyAll(mongoose);
-mongoose.Promise = bluebird;
-mongoose.connect("mongodb://localhost/contactdb", {useMongoClient:true});
+let user = require("./backend/models/user");
+let apiRouter = require("./backend/apirouter");
+let session = require("express-session");
+let mongoStore = require("connect-mongo")(session);
+let localStrategy = require("passport-local");
+let passport = require("passport");
 
 let port = 3001;
 
-let abc = ['a','b','c','d','e','f','g','h','i','j','k','L','M','N','O','P','Q','R','S','T','0','1','2','3','4','5','6','7','8','9'];
+//Setup things
 
-let users = [];
-let loggedUsers = [];
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/KopioGramDB", {useMongoClient:true});
+
+let app = express();
+
+app.use(session({
+	secret: "g2hdSds28Dsf3s",
+	saveUninitialized: false,
+	resave: false,
+	cookie: {maxAge:1000*60*60*24},
+	store: new mongoStore({
+		collection: "session",
+		url: "mongodb://localhost/KopioGramDB",
+		ttl: 24*60*60
+	})
+}))
 
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post("/login", function(req,res){
-	let username = req.body.username;
-	let password = req.body.password;
-	for (i=0; i<users.length; i++){
-		if (username === users[i].username){
-			if (password === users[i].password){
-				let token = "";
-				for (j=0;j<50;j++){
-					let temp = Math.floor(Math.random()*30);
-					token = token + abc[temp];
-				}
-				res.status(200).json({"token":token, "message":"Success"})
-				loggedUsers.push({"username":username, "token":token})
-				console.log("Logged in " +username);
-				return;
-			}
-		}
-	}
-	res.status(403).json({"message":"no such user or wrong password"});
+//Passport setup
+
+passport.serializeUser(function(user,done) {
+	console.log("Serialize user");
+	done(null, user._id);
 });
 
-app.post("/logout", function(req,res){
-	let temptoken = req.body.token;
-	for(i=0;i<loggedUsers.length;i++){
-		if (loggedUsers[i].token === temptoken){
-			let tempusername = loggedUsers[i].username;
-			loggedUsers.splice(i,1);
-			res.status(200).json({"message":"logout success"})
-			console.log("Logged out " +tempusername);
-			return;
+passport.deserializeUser(function(_id,done) {
+	console.log("Deserialize user");
+	user.findOne({"_id":_id}, function(err,user) {
+		if (err) {
+			console.log("Error in deSerializing user");
+		} else {
+			console.log("User found: "+user.username);
+			return done(null, user);
 		}
-	}
-	res.status(404).json({"message":"not found"});
+	});
+		
 });
 
-app.post("/register", function(req, res){
-	let username = req.body.username;
-	let password = req.body.password;
-	for(i=0;i<users.length;i++){
-		if (username === users[i].username){
-			res.status(409).json({"message":"Username taken"});
-			return;
+passport.use("login-local", new localStrategy({
+	usernameField: "username",
+	passwordField: "password",
+	passReqToCallback: true
+} ,function(req, username, password, done) {
+	console.log("login-local");
+	user.findOne({"username":username}, function(err, user) {
+		if(err) {
+			console.log("login-local err");
+			return done(null, false, {"message":"No such user or password"})
+		} 
+		if(!user) {
+			console.log("login-local !user");
+			return done(null, false, {"message":"No such user or password"})
 		}
+		console.log("isPasswordValid "+password);
+		if(!user.isPasswordValid(password)) {
+			console.log("login-local password");
+			return done(null, false, {"message":"No such user or password"});
+		}
+		console.log("done");
+		return done(null, user);
+		});
+}));
+
+function isUserAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	} else {
+		return res.status(403).json({"message":"forbidden"});
 	}
-	let user = {};
-	user.username = username;
-	user.password = password;
-	users.push(user);
-	res.status(200).json({"message":"success"});
-	console.log("Registered " +username);
+}
+
+// Login, logout, register
+
+app.post("/login", 
+	passport.authenticate("login-local",{failureRedirect:'/'}),
+	function(req,res) {
+		req.session.hello = "Hello";
+		res.status(200).json({"message":"success","token":"token"});
+});
 	
-});
-
-app.use("/api", function(req,res,next){
-	let token = req.headers.token;
-	for (i=0; i<loggedUsers.length;i++){
-		if (loggedUsers[i].token === token){
-			next();
+app.post('/register', function(req,res) {
+	console.log("login:"+JSON.stringify(req.body));
+	user.find({"username":req.body.username}, function(err,item) {
+		if(err) {
+			console.log("Register: Finding existing users error");
+			res.status(409).end(JSON.stringify({"message":"username already in use"}));			
 			return;
-		}
+		} else {
+		    console.log("Register: Found existing users:"+JSON.stringify(item));
+			if (item.length > 0) {
+				console.log("More than zero users found");
+				res.status(409).end(JSON.stringify({"message":"username already in use"}));			
+				return;
+			} else {						
+				let temp = new user({});
+				temp.username = req.body.username;
+				temp.password = temp.generateHash(req.body.password);
+				temp.email = req.body.email;
+				temp.role = req.body.role;
+				temp.profileImage = req.body.profileImage;
+				console.log("temp:");
+				console.log(temp);
+				
+				temp.save(function(err,item) {
+					if(err){
+						console.log(err);
+						console.log("Registering already existing username");
+						res.status(409).json({"message":"username already in use"})
+					} else {
+						console.log("User register success");
+						res.status(200).json({"message":"success"});
+					}
+				});
+			}
+		}});
+
+});
+
+app.post("/logout",function(req,res) {
+	if(req.session) {
+		req.logout();
+		req.session.destroy(function(err) {
+			if(err) {
+				console.log("Session destroy problem:"+err);
+			}
+			console.log("Session destroyed");
+		});
+		res.status(200).json({"message":"logout success"});
+		return;
 	}
-	res.status(403).json({"message":"Forbidden"});	
+	res.status(404).json({"message":"bad logout"});
 });
 
-app.post("/api/contactlist", function(req,res){
-	let temp = new contact({
-		"firstName": req.body.firstName,
-		"lastName": req.body.lastName,
-		"email": req.body.email,
-		"phoneNumber": req.body.phoneNumber
-	});
-	temp.save(function(err,item) {
-		if(err){
-			console.log("Failed to save contact");
-			res.status(409).json({"Message":"Failed to save contact"});
-		}else{
-			console.log("Successfully saved contact");
-			res.status(200).json({"Message":"Success"});
-		}
-	});
-});
+// Contact api, middleware to check authentication
 
-app.get("/api/contactlist", function(req,res){
-	contact.find(function(err,contactList){
-		if(err){
-			console.log("Failed to load contactList");
-			res.status(404).json({"Message":"No list found"});
-		}else{
-			console.log("Result in finding contactList");
-			res.status(200).json(contactList);
-		}
-	});
-});
+app.use("/api", isUserAuthenticated, apiRouter);
 
-app.get("/", function(req,res){
-	res.status(200).send("<p>Hello API</p>")
-});
-
-app.delete("/api/contactlist/:id", function(req,res){
-	console.log("Delete contact");
-	let id = req.params.id;
-	contact.remove({"_id":id}, function(err){
-		if (err){
-			console.log("Failed to remove contact");
-			res.status(404).json({"Message":"Failed to remove contact"});
-		}else{
-			console.log("Succesfully removed contact");
-			res.status(200).json({"Message":"Success"});
-		}
-	});
-});
+// Start server and listen
 
 app.listen(port);
 
